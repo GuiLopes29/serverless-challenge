13282477524570b0b0b4466d57b6a55fdb53bf6c
Guilherme <guiplopes29@gmail.com>
2021-12-22 01:56:45 -0300

    try fix

--- serverless/src/data/users/index.js
+++ serverless/src/data/users/index.js
@@ -5,16 +5,14 @@ const sql = require('mysql');
 
 const getAllUsers = async () => {
     try {
-        let pool = await sql.createConnection(config.sql);
+        let pool = sql.createConnection(config.sql);
         const sqlQueries = await utils.loadSqlQueries('users');
-        const getUsers = await pool.query(sqlQueries.getAllUsers)
-        return getUsers.recordset
+        const getUsers = pool.query(sqlQueries.getAllUsers)
+        return getUsers.RowDataPacket
     } catch (error) {
         return error.message;
     }
 }
-
-
 
 module.exports = {
     getAllUsers
